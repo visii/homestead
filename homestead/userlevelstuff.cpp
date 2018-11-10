4625aa1f0bae265372f9968b27c29a4992516fff
Visii <visiiiix@gmail.com>
2018-11-10 12:10:09 -0200

    Made XP required to level up scalable

--- homestead/userlevelstuff.cpp
+++ homestead/userlevelstuff.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "iostuff.h"
 
-
+//Returns or changes the player level.
 int playerLevel(int level)
 {
 	static int playerLevel{ 1 };
@@ -9,7 +9,7 @@ int playerLevel(int level)
 	return playerLevel;
 }
 
-//Returns the amount of AP that should be added at the end of a turn, depending on the level of the player.
+//Returns or changes the amount of AP that should be added at the end of a turn, depending on the level of the player.
 int addAP(int moreAP)
 {
 	static int addAPonLevelUp{ 2 };
@@ -17,15 +17,16 @@ int addAP(int moreAP)
 	return addAPonLevelUp;
 }
 
-//Keeps track of the XP of the player. Whenever XP is an even number, it levels up the player and resets the XP.
-//Possible issues with this are that giving a player 3 XP will not levelup the player. Also it has no scaling.
-//So 2 XP is enough to levelup no matter what level the player is.
+//Keeps track of the XP of the player. Whenever XP is dividable by a scaling number, it levels up the player and resets the XP.
+//Possible issues with this are that giving a player uneven amounts of XP will not levelup the player.
 int playerXP(int addXP)
 {
+	static int levelUpTreshhold{ 2 };
 	static int playerXP{ 0 };
 	playerXP = playerXP + addXP;
-	if ((playerXP % 2 == 0) && (playerXP !=0))
+	if ((playerXP % levelUpTreshhold == 0) && (playerXP !=0))
 	{
+		levelUpTreshhold++;
 		playerLevel(1);
 		addAP(1);
 		playerXP = 0;
