b2f411adbda0422ea85180177320432b7c7b1a13
Visii <visiiiix@gmail.com>
2018-11-11 11:12:18 -0200

    comment

--- homestead/inventory.cpp
+++ homestead/inventory.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 
-//Function to add or remove money from the player.
+// Function to add or remove money from the player. Can also be used to ask
+// how much money.
 int changePlayerMoney(int changeValue)
 {
 	static int playerMoney{ 100 };
